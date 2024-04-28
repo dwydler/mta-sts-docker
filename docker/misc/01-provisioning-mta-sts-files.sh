680e372e26f17da6f62f3cc7da88a2b9cbaa8515
Blackmoon <dwydler@users.noreply.github.com>
2024-04-28 12:37:54 +0200

    Replaced Url for the Code Repository

--- docker/misc/01-provisioning-mta-sts-files.sh
+++ docker/misc/01-provisioning-mta-sts-files.sh
@@ -4,7 +4,7 @@
 mkdir -p /opt/containers/
 
 # Clone repository to local folder
-git clone https://codeberg.org/wd/mta-sts-docker.git /opt/containers/mta-sts/
+git clone https://github.com/dwydler/mta-sts-docker.git /opt/containers/mta-sts/
 
 # Display content of the folder
 ls -lisa /opt/containers/mta-sts/
